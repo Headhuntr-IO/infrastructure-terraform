fe0bbed20af0e09aa954487f56919161071edcba
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-19 14:43:30 +0800

    install metrics server and cluster autoscaler

--- helm_setup.tf
+++ helm_setup.tf
@@ -26,3 +26,30 @@ resource "kubernetes_cluster_role_binding" "tiller" {
     name      = "tiller"
   }
 }
+
+resource "helm_release" "kube_utils" {
+  depends_on = [kubernetes_cluster_role_binding.tiller, kubernetes_service_account.tiller]
+  name       = "kube-utils"
+  chart      = "helm/kube-utils"
+
+  set {
+    name  = "clusterName"
+    value = local.eks_cluster_name
+  }
+}
+
+//resource "helm_release" "application_search_service" {
+//  depends_on = [helm_release.kube_utils, aws_elasticsearch_domain.db_search]
+//  name       = "search-service"
+//  chart      = "helm/search-service"
+//
+//  set {
+//    name  = "config.es.host"
+//    value = aws_elasticsearch_domain.db_search.endpoint
+//  }
+//
+//  set {
+//    name  = "service.annotations.albTags"
+//    value = "hhv2"
+//  }
+//}
--- main.tf
+++ main.tf
@@ -38,6 +38,16 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
+provider "helm" {
+  version = "~> 1.2.1"
+  kubernetes {
+    host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, list("")), 0)
+    cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
+    token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
+    load_config_file       = false
+  }
+}
+
 provider "tls" {
   version = ">= 2.1.1"
 }
